34b943731154d0b74bb6eedfbc748eed9579e79e
Royal Patel <108641849+RoyalPatel95@users.noreply.github.com>
2025-08-27 13:37:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ CORS allow frontend (Vite default ports)
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://online-complier.netlify.app",
     credentials: true,
   })
 );
